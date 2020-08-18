29a2e61cee803b5665eb3d62ceccba3d6a68e509
rhodyapps <rhodyapps@gmail.com>
2020-08-18 08:21:07 -0400

    remove key

--- src/App.js
+++ src/App.js
@@ -18,13 +18,7 @@ import ThingList from "./ThingList";
 var firebaseConfig = {
   
     // Your web app's Firebase configuration
-        apiKey: "AIzaSyAfn2tZz-pF4wv1kTv03BamJrXI8H6rOpM",
-        authDomain: "fb-test-12214.firebaseapp.com",
-        databaseURL: "https://fb-test-12214.firebaseio.com",
-        projectId: "fb-test-12214",
-        storageBucket: "fb-test-12214.appspot.com",
-        messagingSenderId: "169147684678",
-        appId: "1:169147684678:web:2671c9543239db4b64b747"
+        
 
 };
 // Initialize Firebase
